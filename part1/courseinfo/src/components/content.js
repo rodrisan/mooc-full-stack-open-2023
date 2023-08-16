22e5a2a3936a5305edd6dd044b944eaa77f6e2d9
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-08-16 11:30:08 -0600

    feat(part1): refactor for exercise 1.4

--- part1/courseinfo/src/components/content.js
+++ part1/courseinfo/src/components/content.js
@@ -1,12 +1,11 @@
 import { Part } from "./part";
 
-export const Content = ({props}) => {
-  const { part1, part2, part3 } = props;
+export const Content = ({ parts }) => {
   return (
     <>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part1.exercises} />
-      <Part part={part3.name} exercises={part1.exercises} />
+      {parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      ))}
     </>
   );
 };
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,25 +7,30 @@ import { Header } from "./components/header";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
-  const totalExercises = part1.exercises + part2.exercises + part3.exercises;
+  const totalExercises = parts.reduce(
+    (total, part) => total + part.exercises,
+    0
+  );
 
   return (
     <div>
       <Header course={course} />
-      <Content props={{ part1, part2, part3 }} />
+      <Content parts={parts} />
       <Footer sum={totalExercises} />
     </div>
   );
