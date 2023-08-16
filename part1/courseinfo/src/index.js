102378b7a949cadc2be2d6686c5433836ccfcacd
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-08-16 11:31:27 -0600

    feat(part1): refactor for exercise 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,31 +6,33 @@ import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const totalExercises = parts.reduce(
+  const totalExercises = course.parts.reduce(
     (total, part) => total + part.exercises,
     0
   );
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Footer sum={totalExercises} />
     </div>
   );
