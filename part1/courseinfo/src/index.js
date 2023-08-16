af57dd283ab59b3afcf0e736ff70925feb6ad758
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-08-16 11:10:47 -0600

    feat(part1): refactor for exercise 1.1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Content } from "./components/content";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        props={{ part1, part2, part3, exercises1, exercises2, exercises3 }}
+      />
+      <Footer sum={exercises1 + exercises2 + exercises3} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
