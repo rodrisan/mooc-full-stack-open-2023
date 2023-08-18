23808c70d44c63845564fabc0690215466634532
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-08-17 19:02:55 -0600

    feat(part2): refactor for exercise 2.4 step 9

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,17 +2,23 @@ import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
-const Course = ({ course }) => {
-  const { name, parts } = course;
-  const totalExercises = parts.reduce(
-    (total, part) => total + part.exercises,
-    0
-  );
+const Course = ({ courses }) => {
   return (
     <div>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Footer sum={totalExercises} />
+      {courses.map((course) => {
+        const { name, parts } = course;
+        const totalExercises = parts.reduce(
+          (total, part) => total + part.exercises,
+          0
+        );
+        return (
+          <>
+            <Header course={name} />
+            <Content parts={parts} />
+            <Footer sum={totalExercises} />
+          </>
+        );
+      })}
     </div>
   );
 };
