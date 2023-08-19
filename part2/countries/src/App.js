36143aded61c19ab1f9325a307d27d66262de4c6
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-08-18 20:10:19 -0600

    feat(part2): refactor countries for exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState();
+  const [countryWeather, setCountryWeather] = useState();
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [findCountry, setFindCountry] = useState("");
 
@@ -41,26 +43,47 @@ function App() {
       });
   }, []);
 
-  const uniqueCountry =
-    filteredCountries.length === 1 && filteredCountries[0].name.common;
+  const uniqueCountry = filteredCountries.length === 1 && filteredCountries[0];
 
   useEffect(() => {
     uniqueCountry &&
       axios
-        .get(`https://restcountries.com/v3.1/name/${uniqueCountry}`)
+        .get(`https://restcountries.com/v3.1/name/${uniqueCountry.name.common}`)
         .then((response) => {
           setCountry(response.data[0]);
         })
         .catch(() => {
           axios
             .get(
-              `https://studies.cs.helsinki.fi/restcountries/api/name/${uniqueCountry}`
+              `https://studies.cs.helsinki.fi/restcountries/api/name/${uniqueCountry.name.common}`
             )
             .then((response) => {
               setCountry(response.data);
             });
         });
   }, [uniqueCountry]);
+
+  useEffect(() => {
+    const params = {
+      access_key: apiKey,
+      query: uniqueCountry && uniqueCountry.capital[0],
+    };
+    uniqueCountry &&
+      axios
+        .get("http://api.weatherstack.com/current", { params })
+        .then((response) => {
+          if (!response.data.error) {
+            setCountryWeather(response.data);
+          } else {
+            console.log(
+              `Response error: code: ${response.data.error.code}, info: ${response.data.error.info}`
+            );
+          }
+        })
+        .catch((error) => {
+          console.error("An error occurred: ", error);
+        });
+  }, [apiKey, uniqueCountry]);
 
   return (
     <form>
@@ -99,6 +122,14 @@ function App() {
           })}
         {findCountry && !country && filteredCountries.length > 10 && (
           <p>Too many matches, specify another filter</p>
+        )}
+        {countryWeather && uniqueCountry && (
+          <>
+            <h1>Weather in {uniqueCountry.capital[0]}</h1>
+            <p><b>Temperature:</b> {countryWeather.current.temperature} Celsius</p>
+            <img src={countryWeather.current.weather_icons[0]} alt={countryWeather.current.weather_descriptions[0]} />
+            <p><b>Wind:</b> {countryWeather.current.wind_speed} mph, direction {countryWeather.current.wind_dir}</p>
+          </>
         )}
       </div>
     </form>
